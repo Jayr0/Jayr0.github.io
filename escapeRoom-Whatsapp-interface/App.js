2cb1c15cf2d3fff3e9a83affb727790356b834b8
Daan Reuvers <77575704+DaanRVRS@users.noreply.github.com>
2024-02-07 09:40:52 +0100

    created expo app

---
+++ escapeRoom-Whatsapp-interface/App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Bora is een dikke pad</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
